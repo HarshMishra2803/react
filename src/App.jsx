c365641f7b21dbdab9308b722e13b9b1f6f977f9
Harsh Mishra <harsh@Harshs-MacBook-Air.local>
2025-09-16 21:42:34 +0530

    updated react component with cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,26 @@
-import React from 'react'
-import {state, useState} from 'react'
-import Navbar from './components/navbar'
-import Footer from './components/footer'
-
-
+import React from "react";
+import { state, useState } from "react";
+import Navbar from "./components/navbar";
+import Footer from "./components/footer";
+import Card from "./components/card";
 
 const App = () => {
-
   const [count, setCount] = useState(0);
   return (
     <>
-    <Navbar/>
-    <Footer/>
-  
-    </>
-  );
-}
+      <Navbar />
 
-export default App
+      <div className="cards">
+        <Card title="card1"  description ="THis is the description of the card1"/>
+        <Card title="card2"  description ="THis is the description of the card2"/>
+        <Card title="card3"  description ="THis is the description of the card3"/>
+        <Card title="card4"  description ="THis is the description of the card4"/>
+        
+      </div>
+      <Footer />
+    </>
+    // hooks and state in react
+  );
+};
+
+export default App;
